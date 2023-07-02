37681700f377cc03af2b8f1b5ed40b30ef3a9e34
Rahmadillah Maulana <alan14@ce.student.pens.ac.id>
2023-07-03 00:58:24 +0700

    feat: riwayat pemesanan

--- resources/views/pelanggan/pelanggan-riwayat-pemesanan.blade.php
+++ resources/views/pelanggan/pelanggan-riwayat-pemesanan.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.dashboard')
 
+@section('title', 'Riwayat Pemesanan')
+
+@section('page-title', 'Riwayat Pemesanan')
 
 @section('container')
 
@@ -13,22 +16,32 @@
                     <table id="zero_config" class="table table-striped table-bordered">
                         <thead>
                             <tr>
-                                <th>No</th>
-                                <th>ID Pemesanan</th>
-                                <th>Produk</th>
-                                <th>Kuantitas</th>
-                                <th>Total Harga</th>
+                                <th class="text-center">No</th>
+                                <th class="text-center">ID Pemesanan</th>
+                                <th class="text-center">Produk</th>
+                                <th class="text-center">Kuantitas</th>
+                                <th class="text-center">Total Harga</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>AB4123</td>
-                                <td>Pupuk Lumba-Lumba Kemasan 5Kg</td>
-                                <td>10</td>
-                                <td>Rp450000</td>
-                            </tr>
-
+                            @foreach ($pemesanan as $item)
+                                @php
+                                    $iter = $loop->iteration;
+                                @endphp
+                                @foreach ($item->produk as $produk)
+                                <tr>
+                                    @if ($loop->first)
+                                    <td rowspan="{{ $loop->count }}" style="vertical-align: middle" class="text-center">{{ $iter }}</td>
+                                    <td rowspan="{{ $loop->count }}" style="vertical-align: middle" class="text-center">{{ $item->id_keranjang }}</td>
+                                    @endif
+                                    <td style="vertical-align: middle">{{ $produk->nama_produk }}</td>
+                                    <td style="vertical-align: middle" class="text-center">{{ $produk->kuantitas }}</td>
+                                    @if ($loop->first)
+                                    <td rowspan="{{ $loop->count }}" style="vertical-align: middle" class="text-center">Rp{{ number_format($item->total, 0, ',', '.') }}</td>
+                                    @endif  
+                                </tr>
+                                @endforeach
+                            @endforeach
                     </table>
                 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,6 +51,7 @@ Route::middleware('auth:web,pegawai')->group(function () {
     Route::get('/checkout' , [CheckoutController::class, 'index']);
     Route::post('/checkout' , [CheckoutController::class, 'checkout']);
     Route::get('/bayar' , [PembayaranController::class, 'index']);
+    Route::get('/riwayat-pemesanan', [UserController::class, 'riwayatPemesanan']);
 });
 
 Route::get('/register', [RegisterController::class, 'index']);
